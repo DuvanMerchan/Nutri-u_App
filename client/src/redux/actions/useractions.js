c2dcfba453d0eae2c66810790425f3ebf18569b6
Juan Martinez <jemartinezpucheta@gmail.com>
2022-09-30 20:55:43 -0300

    Revisar User y Recipe Slices y Actions

--- client/src/redux/actions/useractions.js
+++ client/src/redux/actions/useractions.js
@@ -1,8 +1,46 @@
 import axios from 'axios'
+import {getUserById, getUserStatus, createUser, deleteUser} from '../userSlice'
 
 //-------------------- RUTAS --------------------------
 const url = ''
 
 
 //-------------------- ACTIONS -----------------------
+export const getUserDetail = async (dispatch) => {
+    try{
+        let res = await axios.get()
+        dispatch(getUserById(res.data))
+    }catch(e){
+        console.log(e)
+    }
+}
 
+
+export const getUserStatus = async (dispatch) => {
+    try{
+        let res = await axios.get()
+        dispatch(getUserStatus(res.data))
+    }catch(e){
+        console.log(e)
+    }
+}
+
+
+export const postUser = async (dispatch) => {
+    try{
+        let res = await axios.post()
+        dispatch(createUser(res.data))
+    }catch(e){
+        console.log(e)
+    }
+}
+
+
+export const deleteUser = async (dispatch) => {
+    try{
+        let res = await axios.delete()
+        dispatch(deleteUser(res.data))
+    }catch(e){
+        console.log(e)
+    }
+}
