b55df2a73ac9b47a5e3a8567cf7c0a7b092c8178
Juan Martinez <jemartinezpucheta@gmail.com>
2022-10-02 00:51:47 -0300

    revisar get db info y get all info

--- api/src/controllers/recipescontrollers.js
+++ api/src/controllers/recipescontrollers.js
@@ -1,6 +1,6 @@
 const axios = require ("axios")
 const { API_KEY } = process.env
-const { Recipe, Diet } = require("../db")
+const { Recipe, Diet } = require("../../db.js")
 // import  dietTypes  from '../utils/apispoon'
 
 
@@ -24,16 +24,65 @@ const getApiRecipes = async() => {
                     healthScore: e.healthScore,
                     image: e.image,
                     summary: e.summary,
-                    cuisines: e.cuisines?.map(ele => ele),
-                    dishTypes: e.dishTypes?.map(ele => ele),
+                    //cuisines: e.cuisines?.map(ele => ele),
+                    //dishTypes: e.dishTypes?.map(ele => ele),
                     diets: e.diets?.map(ele => ele),
                     //ingredients: e.analyzedInstructions[0].steps?.map(ele => ele.ingredients.name): "does not have any ingredient"
             }
-       })
+        })
             return dishRecipe
     }
 
     } catch(error) {
+        console.log(error)
+    }
+}
+
+
+const getDBRecipes = async() => {
+    try {
+        const dbRecipes = await Recipe.findAll({
+            include: [{
+                model: Diet,
+                through: {
+                    attributes: ['name']
+                }
+            }]
+        })
+        console.log(dbRecipes)
+        let data = dbRecipes.map(e => {
+            return {
+                name: e.name,
+                vegetarian: e.vegetarian,
+                vegan: e.vegan,
+                glutenFree: e.glutenFree,
+                dairyFree: e.dairyFree,
+                veryPopular: e.veryPopular,
+                healthScore: e.healthScore,
+                image: e.image,
+                summary: e.summary,
+                diets: e.diets?.map(ele => ele.name)
+            }
+        })
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const getAllInfo = async(req, res) => {
+    try {
+        let data = await getApiRecipes()
+        let dbData = await getDBRecipes()
+        if(!dbData || dbData.length === 0) {
+            if(!data || data.length === 0) {
+                return res.status(404).json({msg: "Oops, we couldn't find any recipes"})
+            }
+            return res.status(200).json(data)
+        }
+        let totalData = [...data, ...dbData]
+        return res.status(200).json(totalData)
+    } catch (error) {
         console.log(error)
     }
 }
@@ -58,8 +107,8 @@ const getApiNameRecipes = async(name) => {
                     healthScore: e.healthScore,
                     image: el.image,
                     summary: el.summary,
-                    cuisines: el.cuisines?.map(ele => ele),
-                    dishTypes: el.dishTypes?.map(ele => ele),
+                    //cuisines: el.cuisines?.map(ele => ele),
+                    //dishTypes: el.dishTypes?.map(ele => ele),
                     diets: el.diets?.map(ele => ele),
                     //ingredients: el.analyzedInstructions[0].steps?.map(ele => ele.ingredients.name): "does not have any ingredient"
                 }
