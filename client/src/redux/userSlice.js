1b9dafe9481919965bb498e3bf8cf5ee1053175f
facoogle <facoogle@gmail.com>
2022-10-05 01:29:16 -0300

    Ya funciona registro de usuarios

--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -1,30 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
 
-export const userSlice = createSlice({
-    name: 'user',
-    initialState:{
-        user:{},
-        logged: false,
-    },
-    reducers:{
-        getUserById: (state, action)=>{
-            state.user = action.payload
-        },
-        getUserStatus: (state, action)=>{
-            state.logged = action.payload
-        },
-        createUser: (state, action)=>{
-            state.user = [...state.user, action.payload]
-        },
-        deleteUser: (state, action)=>{
-            state.user = action.payload
-        },
-    }
-})
+// export const userSlice = createSlice({
+//     name: 'user',
+//     initialState:{
+//         user:{},
+//         logged: false,
+//     },
+//     reducers:{
+//         getUserById: (state, action)=>{
+//             state.user = action.payload
+//         },
+//         getUserStatus: (state, action)=>{
+//             state.logged = action.payload
+//         },
+//         createUser: (state, action)=>{
+//             state.user = [...state.user, action.payload]
+//         },
+//         deleteUser: (state, action)=>{
+//             state.user = action.payload
+//         },
+//     }
+// })
 
 
 
-export const {getUserById, getUserStatus, createUser, deleteUser} = recipesSlice.actions
+// export const {getUserById, getUserStatus, createUser, deleteUser} = recipesSlice.actions
 
-export default recipesSlice.reducer
+// export default recipesSlice.reducer
