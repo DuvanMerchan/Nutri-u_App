891699d6a351b69481c16a8f13d5392cd1833b2d
facoogle <facoogle@gmail.com>
2022-10-09 10:31:06 -0300

    forgot

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const dietTypes = require('./src/utils/apispoon')
 // import dietTypes from './src/utils/apispoon'
 
 // Syncing all the models at once.
-conn.sync({ alter: true }).then(() => {
+conn.sync({ alter: false }).then(() => {
   server.listen(DB_PORT, () => {
     console.log(`%s listening at ${DB_PORT}`); // eslint-disable-line no-console
  
