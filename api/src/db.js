9d33912743149fff0e029e18d4b8a5f983a47775
facoogle <facoogle@gmail.com>
2022-10-13 09:21:07 -0300

    modified db

--- api/src/db.js
+++ api/src/db.js
@@ -3,13 +3,44 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${process.env.DB_USER || DB_USER}:${process.env.DB_PASSWORD || DB_PASSWORD}@${process.env.DB_HOST || DB_HOST}/${process.env.DB_NAME || "nutri-u"}`, {
-  logging: false,
-  native: false, 
-});
+
+
+let sequelize =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize({
+        database: DB_NAME || "nutri-u",
+        dialect: "postgres",
+        host: DB_HOST,
+        port: 5432,
+        username: DB_USER,
+        password: DB_PASSWORD,
+        pool: {
+          max: 3,
+          min: 1,
+          idle: 10000,
+        },
+        dialectOptions: {
+          ssl: {
+            require: true,
+            // Ref.: https://github.com/brianc/node-postgres/issues/2009
+            rejectUnauthorized: false,
+          },
+          keepAlive: true,
+        },
+        ssl: true,
+      })
+    : new Sequelize(
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME||"nutri-u"}`, //DB NAME countries
+        { logging: false, native: false }
+      );
+
+// const sequelize = new Sequelize(`postgres://${process.env.DB_USER || DB_USER}:${process.env.DB_PASSWORD || DB_PASSWORD}@${process.env.DB_HOST || DB_HOST}/${process.env.DB_NAME || "nutri-u"}`, {
+//   logging: false,
+//   native: false, 
+// });
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
